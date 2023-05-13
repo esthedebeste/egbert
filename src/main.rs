f0f78c3fa145e8f4e090f24193d20844e0942e1f
esthedebeste <57283066+esthedebeste@users.noreply.github.com>
2023-05-13 14:57:07 +0200

    Added "Loading..." indicator & removed pregenerated first image

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,7 @@
+use gloo_timers::future::TimeoutFuture;
 use rand::rngs::StdRng;
 use rand::SeedableRng;
+use sycamore::futures::spawn_local_scoped;
 use sycamore::rt::JsCast;
 use sycamore::{prelude::*, rt::JsValue};
 use web_sys::{CanvasRenderingContext2d, HtmlCanvasElement};
@@ -32,30 +34,38 @@ fn main() {
         );
         let display_colors = create_signal(cx, vec![&COLORS[3], &COLORS[5], &COLORS[1]]);
         let canvas = create_node_ref(cx);
-        let another = |_| {
-            let canvas: DomNode = canvas.get();
-            let canvas: HtmlCanvasElement = canvas.to_web_sys().dyn_into().unwrap();
-            let context: CanvasRenderingContext2d = canvas
-                .get_context("2d")
-                .unwrap()
-                .unwrap()
-                .dyn_into()
-                .unwrap();
-            let mut rng = StdRng::from_entropy();
-            let (rules, colors, img) = generate(WIDTH, HEIGHT, &mut rng);
-            for y in 0..HEIGHT {
-                for x in 0..WIDTH {
-                    let color = &img[y * WIDTH + x];
-                    context.set_fill_style(&JsValue::from_str(&color.to_hex()));
-                    context.fill_rect(x as f64, y as f64, 1.0, 1.0);
+        let generating = create_signal(cx, true);
+        let another = move || {
+            generating.set(true);
+            spawn_local_scoped(cx, async move {
+                TimeoutFuture::new(0).await;
+                let canvas: DomNode = canvas.get();
+                let canvas: HtmlCanvasElement = canvas.to_web_sys().dyn_into().unwrap();
+                let context: CanvasRenderingContext2d = canvas
+                    .get_context("2d")
+                    .unwrap()
+                    .unwrap()
+                    .dyn_into()
+                    .unwrap();
+                let mut rng = StdRng::from_entropy();
+                let (rules, colors, img) = generate(WIDTH, HEIGHT, &mut rng);
+                for y in 0..HEIGHT {
+                    for x in 0..WIDTH {
+                        let color = &img[y * WIDTH + x];
+                        context.set_fill_style(&JsValue::from_str(&color.to_hex()));
+                        context.fill_rect(x as f64, y as f64, 1.0, 1.0);
+                    }
                 }
-            }
-            display_colors.set(colors);
-            display_rules.set(rules);
+                display_colors.set(colors);
+                display_rules.set(rules);
+                generating.set(false);
+            });
         };
+        on_mount(cx, another);
         view! { cx,
             canvas(ref=canvas, width=WIDTH, height=HEIGHT)
-            button(on:click=another){ "🔁 Another?" }
+            button(on:click=move |_| another(), disabled=*generating.get()){ "🔁 Another?" }
+            p(id="generating", aria-hidden=!*generating.get()){ "Generating..." }
             h2 { "Colors:" }
             p { (display_colors.get().iter().map(|c| c.name).collect::<Vec<_>>().join("")) }
             h2 { "Rules:" }
